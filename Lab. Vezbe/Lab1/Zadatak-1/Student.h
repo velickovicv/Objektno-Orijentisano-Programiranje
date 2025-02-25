1d24d149714ab873e535a5eb48b842036aa23e85
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-02-25 14:01:13 +0100

    Update Student.h

--- Lab. Vezbe/Lab1/Zadatak-1/Student.h
+++ Lab. Vezbe/Lab1/Zadatak-1/Student.h
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
 
+class Student
+{
+private:
+	char* ime;
+	char* prezime;
+	int brojIndexa;
+	int maxBrojIspita;
+	int brojPolozenihIspita;
+	int* ocene;
+
+public:
+	Student();
+	Student(char* ime, char* prezime, int brojIndexa, int maxBrojIspita
+	~Student();
+	inline void info(int* index, int* ispiti)
+	{
+		*index = this->brojIndexa;
+		*ispiti = this->brojPolozenihIspita;
+	}
+	void dodajOcenu();
+	void input();
+	void prikaziOcena();
+	void output();
+	void azuriraj();
+
+};
