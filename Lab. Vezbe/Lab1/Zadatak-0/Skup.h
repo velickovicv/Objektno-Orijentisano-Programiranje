e77b143b0eb01eb694bd77d6749076fc816e472b
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-02-25 13:01:03 +0100

    Create Skup.h

---
+++ Lab. Vezbe/Lab1/Zadatak-0/Skup.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Skup
+{
+ private:
+
+	int n;
+	int* niz;
+
+ public:
+
+	Skup();
+	Skup(int n);
+	~Skup();
+	inline int brojElemenata() { return n; };
+	void izbaciDuplikat();
+	int ispitaj(int element);
+	void input();
+	void nerastuci();
+	void output();
+};
