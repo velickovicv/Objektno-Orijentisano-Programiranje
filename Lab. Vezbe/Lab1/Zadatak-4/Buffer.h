a797cd664bd195413664604e9acb2acdfc41f3f8
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-02-26 13:43:02 +0100

    Update Buffer.h

--- Lab. Vezbe/Lab1/Zadatak-4/Buffer.h
+++ Lab. Vezbe/Lab1/Zadatak-4/Buffer.h
@@ -1,1 +1,25 @@
+#pragma once
+using namespace std;
 
+class Buffer
+{
+private:
+
+	int* niz;
+	int velicina;
+	int brUpisanih;
+
+public:
+
+	Buffer();
+	Buffer(int velicina);
+	Buffer(const Buffer& b)
+	~Buffer();
+	inline int vratiBrEl() { return brUpisanih; }
+	void push(int podatak);
+	int pop();
+	void prikazi();
+	void dupliraj();
+	void izbaciPrazna();
+
+};
