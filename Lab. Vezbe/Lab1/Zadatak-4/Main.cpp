a7a9106844edb5090249e950de3da7f336924ef1
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-03-25 11:21:39 +0100

    Update Main.cpp

--- Lab. Vezbe/Lab1/Zadatak-4/Main.cpp
+++ Lab. Vezbe/Lab1/Zadatak-4/Main.cpp
@@ -9,17 +9,21 @@ int main()
 	cin >> n;
 
 	Buffer b1(n);
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; ++i)
 		b1.push(i);
 
 	Buffer* b2 = new Buffer(b1);
 
-	cout << "m: ";
+	cout << "Unesite m: ";
 	int m;
 	cin >> m;
-	for (int i = 0; i < m; i++)
-		b1.pop();
 
-	delete b2;
+	for (int i = 0; i < m; ++i)
+		b1.pop();
+    
+	cout << "br el u b1: " << b1.vratiBrEl(); << endl;  // Pristup je sa ( . ) jer se radi o statickoj memoriji !!
+	cout << "br el u b2: " << b2->vratiBrEl();  // Pristup je sa this -> jer se radi o dinamickoj memoriji !!
+
+	delete b2; // Oslobadjamo se dinamicke memorije
 	return 0;
 }
