a2483c68fd38ec776641c6f566a0c705a41c7a93
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-03-25 11:07:04 +0100

    Update Main.cpp

--- Lab. Vezbe/Lab1/Zadatak-4/Main.cpp
+++ Lab. Vezbe/Lab1/Zadatak-4/Main.cpp
@@ -1,1 +1,25 @@
+#include "Buffer.h"
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	cout << "Upisite n: ";
+	int n;
+	cin >> n;
+
+	Buffer b1(n);
+	for (int i = 1; i <= n; i++)
+		b1.push(i);
+
+	Buffer* b2 = new Buffer(b1);
+
+	cout << "m: ";
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+		b1.pop();
+
+	delete b2;
+	return 0;
+}
