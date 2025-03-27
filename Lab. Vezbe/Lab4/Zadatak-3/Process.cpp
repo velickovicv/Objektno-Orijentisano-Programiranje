25da7421922ed829a186052cbfa1fbe3d103cbbd
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-03-27 10:37:07 +0100

    Update Process.cpp

--- Lab. Vezbe/Lab4/Zadatak-3/Process.cpp
+++ Lab. Vezbe/Lab4/Zadatak-3/Process.cpp
@@ -1,1 +1,24 @@
+#include "Process.h"
+#include <iostream>
+using namespace std;
 
+Process::Process()
+{
+	sifra = 0;
+	stanje = 0;
+	tip = 0
+}
+Process::Process(int ssifra, int ttip)
+{
+	this->sifra = ssifra;
+	this->tip = ttip;
+	this->stanje = 0;
+}
+Process::~Process()
+{
+
+}
+void Process::prikazi()
+{
+	cout << "Sifra: " << sifra << ", Stanje: " << stanje, << ", Tip: " << tip << endl;
+}
