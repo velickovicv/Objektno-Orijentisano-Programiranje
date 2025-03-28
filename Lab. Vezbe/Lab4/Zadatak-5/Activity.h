225bb210fe0110fe5a51839f2a259f3e88990126
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-03-28 11:35:11 +0100

    Create Activity.h

---
+++ Lab. Vezbe/Lab4/Zadatak-5/Activity.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Step.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+class Activity : public Step
+{
+private:
+
+	int obavezna;
+
+public:
+
+	Activity();
+	Activity(char* nnaziv, int o);
+	virtual ~Activity();
+	Activity(Step* a) : Step(a) {
+		obavezna = a->GetSpecAttribute();
+	}
+	virtual int GetSpecAttribute() { return obavezna; }
+	virtual void Prikazi(ostream& izlaz);
+	friend istream& operator >>(istream& ulaz, Activity& a);
+
+};
