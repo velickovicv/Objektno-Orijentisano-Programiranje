7ffdae2a0ac79d0627e823cf4b7cab52ea26176d
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-03-29 12:19:09 +0100

    Update Predmet.cpp

--- Blanketi/Kolokvijum I 2021./Predmet.cpp
+++ Blanketi/Kolokvijum I 2021./Predmet.cpp
@@ -1,28 +1,3 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-class Predmet
-{
-private:
-
-	double masa;
-	double vrednost;
-
-public:
-
-	Predmet();
-	Predmet(double m, double v);
-	virtual ~Predmet();
-
-	// Sve funkcije su inline sto znaci da se svaka funkcija se implementira u svojim .cpp fajlovima!!
-
-	virtual void vratiVrednost();
-	virtual void vratiMasu();
-	virtual void prikazi(ostream& izlaz) = 0; // Apstrakta funkcija
-};
-
 #include <Predmet.h>
 
 Predmet::Predmet()
@@ -45,4 +20,3 @@ void Predmet::vratiVrednost()
 void Predmet::vratiMasu()
 {
 	return masa;
-}
