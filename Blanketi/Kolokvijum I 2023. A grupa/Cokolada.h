f29f660f036323a57c3be32fda05a83951151779
velickovicv <160411101+velickovicv@users.noreply.github.com>
2025-05-19 16:06:09 +0200

    Update Cokolada.h

--- Blanketi/Kolokvijum I 2023. A grupa/Cokolada.h
+++ Blanketi/Kolokvijum I 2023. A grupa/Cokolada.h
@@ -12,14 +12,9 @@ public:
 
 	Cokolada();
 	Cokolada(double _cena, int _brojMasnica, double _procenatKakaa);
-
-	// Ako se trazi u zadatku a trazi se, da napravimo kopiju paketica
-	// to znaci da sve klase koje se nalaze u tom paketicu treba da imaju copy construktor 
-
-	Cokolada(const Cokolada& c);  // Copy construktor 
-	virtual ~Cokolada();
-
-	void prikazi(ostream& izlaz) const;
-
+	Cokolada(const Cokolada& c);
+	~Cokolada();
+    
+	void print(ostream& izlaz) const;
 	int vratiBrojMasnica() const;
 };
